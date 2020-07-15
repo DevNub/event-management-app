bde6bbb5c928827180efeb250e50358977aea11b
DevNub <ddac_dan@hotmail.com>
2020-07-15 16:30:30 -0400

    responsive mobile/desktop view, changed popover

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ScreensizeService } from '../services/screensize.service';
+import { PopoverController } from '@ionic/angular';
+import { PopoverComponentPage } from '../popover-component/popover-component.page';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  isDesktop: boolean;
+  constructor(private screensizeService: ScreensizeService,public popover:PopoverController) {
+    this.screensizeService.isDesktopView().subscribe(isDesktop => {
+      console.log('Is desktop changed: ', isDesktop);
+      this.isDesktop = isDesktop;
+    });
+  }
 
-  constructor() {}
+  async CreatePopover(ev: any) {
+    const popover = await this.popover.create({
+      component: PopoverComponentPage,
+      cssClass: 'my-custom-class',
+      event: ev,
+      translucent: true
+    });
+    return await popover.present();
+  }
 
 }
